2db57ad8396d9fdfe0eb469b9dc33cf35a02d5b8
thu_san <tunlinthu94@gmail.com>
2019-01-16 22:45:25 +0900

    Add Typescript

--- src/createAction.ts
+++ src/createAction.ts
@@ -3,49 +3,125 @@
  * @param actionName Prefix for action
  */
 export const createAction = <
+  TriggerAction = undefined,
   TriggerPayload = undefined,
-  RequestPayload = undefined,
+  SuccessAction = undefined,
   SuccessPayload = undefined,
+  FailureAction = undefined,
   FailurePayload = undefined,
-  FulfillPayload = undefined
+  FulfillAction = undefined,
+  FulfillPayload = undefined,
+  RequestAction = undefined,
+  RequestPayload = undefined
 >(
   actionName: string
 ) => {
-  const triggerAction = `${actionName}/TRIGGER`;
+  type action = {
+    (payload: TriggerPayload): {
+      type: TriggerAction;
+      payload: TriggerPayload;
+    };
+    TRIGGER: TriggerAction;
+    SUCCESS: SuccessAction;
+    FAILURE: FailureAction;
+    FULFILL: FulfillAction;
+    REQUEST: RequestAction;
+    trigger: (
+      payload: TriggerPayload
+    ) => {
+      type: TriggerAction;
+      payload: TriggerPayload;
+    };
+    success: (
+      payload: SuccessPayload
+    ) => {
+      type: SuccessAction;
+      payload: SuccessPayload;
+    };
+    failure: (
+      payload: FailurePayload
+    ) => {
+      type: FailureAction;
+      payload: FailurePayload;
+    };
+    fulfill: (
+      payload: FulfillPayload
+    ) => {
+      type: FulfillAction;
+      payload: FulfillPayload;
+    };
+    request: (
+      payload: RequestPayload
+    ) => {
+      type: RequestAction;
+      payload: RequestPayload;
+    };
+    /**
+     * @property Property to retrieve type and assign in reducer action
+     */
+    actionTypes:
+      | {
+          type: TriggerAction;
+          payload: TriggerPayload;
+        }
+      | {
+          type: SuccessAction;
+          payload: SuccessPayload;
+        }
+      | {
+          type: FailureAction;
+          payload: FailurePayload;
+        }
+      | {
+          type: FulfillAction;
+          payload: FulfillPayload;
+        }
+      | {
+          type: RequestAction;
+          payload: RequestPayload;
+        };
+  };
 
-  const action = (payload?: TriggerPayload) => ({
+  const triggerAction: TriggerAction = (`${actionName}/TRIGGER` as unknown) as TriggerAction;
+
+  const action: action = (payload: TriggerPayload) => ({
     type: triggerAction,
     payload
   });
 
   // action types
   action.TRIGGER = triggerAction;
-  action.REQUEST = `${actionName}/REQUEST`;
-  action.SUCCESS = `${actionName}/SUCCESS`;
-  action.FAILURE = `${actionName}/FAILURE`;
-  action.FULFILL = `${actionName}/FULFILL`;
+  action.SUCCESS = (`${actionName}/SUCCESS` as unknown) as SuccessAction;
+  action.FAILURE = (`${actionName}/FAILURE` as unknown) as FailureAction;
+  action.FULFILL = (`${actionName}/FULFILL` as unknown) as FulfillAction;
+  action.REQUEST = (`${actionName}/REQUEST` as unknown) as RequestAction;
 
   // actions
-  action.trigger = (payload?: TriggerPayload) => ({
+  action.trigger = (payload: TriggerPayload) => ({
     type: triggerAction,
     payload
   });
-  action.request = (payload?: RequestPayload) => ({
-    type: action.REQUEST,
-    payload
-  });
-  action.success = (payload?: SuccessPayload) => ({
+  action.success = (payload: SuccessPayload) => ({
     type: action.SUCCESS,
     payload
   });
-  action.failure = (payload?: FailurePayload) => ({
+  action.failure = (payload: FailurePayload) => ({
     type: action.FAILURE,
     payload
   });
-  action.fulfill = (payload?: FulfillPayload) => ({
+  action.fulfill = (payload: FulfillPayload) => ({
     type: action.FULFILL,
     payload
   });
+  action.request = (payload: RequestPayload) => ({
+    type: action.REQUEST,
+    payload
+  });
+
+  action.actionTypes = {
+    type: triggerAction,
+    payload: (undefined as unknown) as TriggerPayload
+  };
 
   return action;
 };
